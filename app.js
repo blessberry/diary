b28b9a3a77697b195f52c5c2915791a51c174ada
blessberry <46441946+blessberry@users.noreply.github.com>
2019-11-07 10:46:14 +0200

    chore(return token): return token on signup
    - update package.json
    - update app.js
    
    [finishes #169555100]

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import routes from './API/routes/routes';
 import responses from './API/helpers/responses';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
